81c2b94dd5ad620227a1e8c58c9e93daf642ccbf
André Francisco <fratelli@Andres-MacBook-Air.local>
2016-05-26 18:30:33 -0400

    Initial commit

---
+++ HypeChatDemo/ChatViewController.m
@@ -0,0 +1,173 @@
+//
+// The MIT License (MIT)
+// Copyright (c) 2016 Hype Labs Ltd
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#import "ChatViewController.h"
+#import "MessageTableViewCell.h"
+
+#import <Hype/Hype.h>
+
+@interface ChatViewController () <UITableViewDataSource, StoreDelegate>
+
+@end
+
+@implementation ChatViewController
+
+@synthesize store = _store;
+
+- (IBAction)didTapSendButton:(id)sender
+{
+    NSString * text = self.textView.text;
+    
+    if (text.length > 0) {
+        
+        NSError * error;
+        
+        // When sending content there must be some sort of protocol that both parties
+        // understand. In this case, we simply send the text encoded in UTF8. The data
+        // must be decoded when received, using the same encoding.
+        NSData * data = [text dataUsingEncoding:NSUTF8StringEncoding];
+        
+        HYPMessage * message = [[HYP instance] sendData:data
+                                             toInstance:self.store.instance
+                                                  error:&error];
+        
+        if (error) {
+            
+            // The message couldn't be sent for some reason. Common causes include the
+            // instance not being reachable anymore (usualy resulting in it being lost).
+            // This error should be properly handled, probably by notifying the user
+            // that the delivery was not possible.
+            return;
+        }
+        
+        else {
+            
+            // Clear the input view
+            self.textView.text = @"";
+            
+            // Adding the message to the store updates the table view
+            [self.store addMessage:message];
+        }
+    }
+}
+
+- (void)store:(Store *)store didAddMessage:(HYPMessage *)message
+{
+    // Reloads the data and scrolls the table to the bottom. The UX for this is not
+    // very good if there are not enough messags to fill the table, but it's nice
+    // otherwise.
+    [self.messageDisplay reloadData];
+    [self.messageDisplay scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:self.store.allMessages.count - 1 inSection:0]
+                               atScrollPosition:UITableViewScrollPositionBottom
+                                       animated:YES];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self.messageDisplay setDataSource:self];
+}
+
+- (Store *)store
+{
+    return _store;
+}
+
+- (void)setStore:(Store *)store
+{
+    _store = store;
+    _store.delegate = self;
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    // Sets all messages as read
+    self.store.lastReadIndex = self.store.allMessages.count;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.store.allMessages.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    MessageTableViewCell * aCell = [tableView dequeueReusableCellWithIdentifier:@"MessageTableViewCell"];
+    HYPMessage * message = [self.store.allMessages objectAtIndex:indexPath.row];
+    
+    if (aCell == nil) {
+        
+        aCell = [MessageTableViewCell cellWithMessage:message];
+    }
+    
+    else {
+        
+        aCell.message = message;
+    }
+    
+    // Initialize the cell
+    aCell.textView.text = [[NSString alloc] initWithData:message.data
+                                                encoding:NSUTF8StringEncoding];
+    
+    return aCell;
+}
+
+- (IBAction)didRecognizeTapGesture:(id)sender
+{
+    [self.textView resignFirstResponder];
+}
+
+- (void)keyboardWillShow:(NSNotification *)notification
+{
+    CGSize keyboardSize = [[[notification userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        CGRect f = self.view.frame;
+        f.origin.y = -keyboardSize.height;
+        self.view.frame = f;
+    }];
+}
+
+-(void)keyboardWillHide:(NSNotification *)notification
+{
+    [UIView animateWithDuration:0.3 animations:^{
+        CGRect f = self.view.frame;
+        f.origin.y = 0.0f;
+        self.view.frame = f;
+    }];
+}
+
+@end
---
+++ HypeChatDemo/ContactViewController.m
@@ -0,0 +1,236 @@
+//
+// The MIT License (MIT)
+// Copyright (c) 2016 Hype Labs Ltd
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#import "ContactViewController.h"
+#import "ContactTableViewCell.h"
+#import "ChatViewController.h"
+#import "Store.h"
+
+#import <Hype/Hype.h>
+
+@interface ContactViewController () <HYPObserver, UITableViewDataSource>
+
+// The stores object keeps track of message storage associated with each instance (peer)
+@property (strong, atomic, readonly) NSMutableDictionary * stores;
+@property (strong, atomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation ContactViewController
+
+@synthesize tableView = _tableView;
+@synthesize stores = _stores;
+
+- (NSMutableDictionary *)stores
+{
+    @synchronized(self) {
+        
+        if (_stores == nil) {
+            _stores = [NSMutableDictionary new];
+        }
+        
+        return _stores;
+    }
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Request Hype to start when the view loads. If this is successful the table
+    // should start displaying other peers soon. Notice that it's OK to request the
+    // framework to start if it's already running, so this method can be called
+    // several times.
+    [self requestHypeToStart];
+}
+
+- (void)requestHypeToStart
+{
+    // Adding self as an Hype observer makes sure that the contact view controller gets
+    // notifications for events being triggered by the Hype framework. These events
+    // include the framework's lifecycle (starting, stopping), network events (finding
+    // and losing instances), and message I/O.
+    [[HYP instance] addHypeObserver:self];
+    
+    // Requesting Hype to start is equivalent to requesting the device to publish
+    // itself on the network and start browsing for other devices in proximity. If
+    // everything goes well, the -hypeDidStart: delegate method gets called, indicating
+    // that the device is actively participating on the network. The 00000000 realm is
+    // reserved for test apps, so it's not recommended that apps are shipped with it.
+    // For generating a realm go to https://hypelabs.io, login, access the dashboard
+    // under the Apps section and click "Create New App". The resulting app should
+    // display a realm number. Copy and paste that here.
+    [[HYP instance] startWithOptions:@{
+                                       HYPOptionRealmKey:@"00000000"
+                                       }];
+}
+
+- (void)hypeDidStart:(HYP *)hype
+{
+    // At this point, the device is actively participating on the network. Other devices
+    // (instances) can be found at any time and the domestic (this) device can be found
+    // by others. When that happens, the two devices should be ready to communicate.
+    NSLog(@"Hype started!");
+}
+
+- (void)hypeDidStop:(HYP *)hype
+              error:(NSError *)error
+{
+    // The framework has stopped working for some reason. If it was asked to do so (by
+    // calling -stop) the error parameter is nil. If, on the other hand, it was forced
+    // by some external means, the error parameter indicates the cause. Common causes
+    // include the user turning the Bluetooth and/or Wi-Fi adapters off. When the later
+    // happens, you shouldn't attempt to start the Hype services again. Instead, the
+    // framework triggers a -hypeDidBecomeReady: delegate method if recovery from the
+    // failure becomes possible.
+    NSLog(@"Hype stoped [%@]", [error localizedDescription]);
+}
+
+- (void)hypeDidFailStarting:(HYP *)hype
+                      error:(NSError *)error
+{
+    // Hype couldn't start its services. Usually this means that all adapters (Wi-Fi
+    // and Bluetooth) are not on, and as such the device is incapable of participating
+    // on the network. The error parameter indicates the cause for the failure. Attempting
+    // to restart the services is futile at this point. Instead, the implementation should
+    // wait for the framework to trigger a -hypeDidBecomeReady: notification, indicating
+    // that recovery is possible, and start the services then.
+    NSLog(@"Hype failed starting [%@]", error.localizedDescription);
+}
+
+- (void)hypeDidBecomeReady:(HYP *)hype
+{
+    // This Hype delegate event indicates that the framework believes that it's capable
+    // of recovering from a previous start failure. This event is only triggered once.
+    // It's not guaranteed that starting the services will result in success, but it's
+    // known to be highly likely. If the services are not needed at this point it's
+    // possible to delay the execution for later, but it's not guaranteed that the
+    // recovery conditions will still hold by then.
+    [self requestHypeToStart];
+}
+
+- (void)hype:(HYP *)hype didFindInstance:(HYPInstance *)instance
+{
+    // Hype instances that are participating on the network are identified by a full
+    // UUID, composed by the vendor's realm followed by a unique identifier generated
+    // for each instance.
+    NSLog(@"Found instance: %@", instance.stringIdentifier);
+    
+    // Instances should be strongly kept by some data structure. Their identifiers
+    // are useful for keeping track of which instances are ready to communicate.
+    [self.stores setObject:[Store storeWithInstsance:instance]
+                    forKey:instance.stringIdentifier];
+    
+    // Reloading the table reflects the change
+    [self.tableView reloadData];
+}
+
+- (void)hype:(HYP *)hype didLoseInstance:(HYPInstance *)instance
+       error:(NSError *)error
+{
+    // An instance being lost means that communicating with it is no longer possible.
+    // This usually happens by the link being broken. This can happen if the connection
+    // times out or the device goes out of range. Another possibility is the user turning
+    // the adapters off, in which case not only are all instances lost but the framework
+    // also stops with an error.
+    NSLog(@"Lost instance: %@ [%@]", instance.stringIdentifier, error.localizedDescription);
+    
+    // Cleaning up is always a good idea. It's not possible to communicate with instances
+    // that were previously lost.
+    [self.stores removeObjectForKey:instance.stringIdentifier];
+    
+    // Reloading the table reflects the change
+    [self.tableView reloadData];
+}
+
+- (void)hype:(HYP *)hype didReceiveMessage:(HYPMessage *)message fromInstance:(HYPInstance *)instance
+{
+    NSLog(@"Got a message from: %@", instance.stringIdentifier);
+    
+    Store * store = [self.stores objectForKey:instance.stringIdentifier];
+    
+    // Storing the message triggers a reload update in the chat view controller
+    [store addMessage:message];
+    
+    // The data is reloaded so the green circle indicator is shown for contacts that have new
+    // messages. Reloading is probably an overkill, but the point is to maintain focus on how
+    // the framework works.
+    [self.tableView reloadData];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.stores count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    ContactTableViewCell * aCell = [tableView dequeueReusableCellWithIdentifier:@"ContactTableViewCell"];
+    
+    // Assuming that order is not relevant
+    Store * store = [self.stores.allValues objectAtIndex:indexPath.row];
+    
+    if (aCell == nil) {
+        
+        aCell = [ContactTableViewCell cellWithStore:store];
+    }
+    
+    else {
+        
+        aCell.store = store;
+    }
+    
+    // Configure the cell to display information from the found instance. The description is
+    // a feature that has not yet been made available by the framework, which consists of
+    // each peer putting an "announcement" on the network that helps identifying the
+    // running instance.
+    aCell.displayName.text = store.instance.stringIdentifier;
+    aCell.details.text = @"Description not available";
+    aCell.contentIndicator.hidden = !store.hasNewMessages;
+    
+    [aCell setNeedsLayout];
+    
+    return aCell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 100;
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    ChatViewController * vc = [segue destinationViewController];
+    
+    // Pass the store along when the segue executes
+    [vc setStore:((ContactTableViewCell *)sender).store];
+    
+    vc.instanceIdentifier.text = vc.store.instance.stringIdentifier;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [self.tableView reloadData];
+}
+
+@end
